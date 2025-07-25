e5d1733b2314085cd69b108250b080d6d3ff17b4
ashish-jaypee <ashish.chaudhary@jaypeebrothers.com>
2025-07-25 19:13:03 +0530

    vercel and vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -6,40 +6,31 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            src: resolve(__dirname, 'src'),
+  base: '/dashboard/',
+  resolve: {
+    alias: {
+      src: resolve(__dirname, 'src'),
+    },
+  },
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.tsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-tsx',
+          setup(build) {
+            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+              loader: 'tsx',
+              contents: await fs.readFile(args.path, 'utf8'),
+            }));
+          },
         },
+      ],
     },
-    esbuild: {
-        loader: 'tsx',
-        include: /src\/.*\.tsx?$/,
-        exclude: [],
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            plugins: [
-                {
-                    name: 'load-js-files-as-tsx',
-                    setup(build) {
-                        build.onLoad(
-                            { filter: /src\\.*\.js$/ },
-                            async (args) => ({
-                                loader: 'tsx',
-                                contents: await fs.readFile(args.path, 'utf8'),
-                            })
-                        );
-                    },
-                },
-            ],
-        },
-    },
-
-
-    
-    // plugins: [react(),svgr({
-    //   exportAsDefault: true
-    // })],
-
-    plugins: [svgr(), react()],
+  },
+  plugins: [svgr(), react()],
 });
